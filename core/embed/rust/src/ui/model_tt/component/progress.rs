fad653a38b13184e76f6fb7608339623a369cf5e
matejcik <ja@matejcik.cz>
2023-11-08 13:17:11 +0100

    fix(core/ui): repaint the title of Progress layout when requested

--- core/embed/rust/src/ui/model_tt/component/progress.rs
+++ core/embed/rust/src/ui/model_tt/component/progress.rs
@@ -87,6 +87,9 @@ impl Component for Progress {
                     }
                 });
             }
+        } else {
+            self.title.event(ctx, event);
+            self.description.event(ctx, event);
         }
         None
     }
