fdddfe0f3a8f10a0083d46ee44a889535e24408a
M1nd3r <petrsedlacek.km@seznam.cz>
2024-04-05 12:59:52 +0200

    fix(core): fix loading of sessions from cache, improve logging
    Improve logging of expected message type in session context

--- core/src/trezor/wire/thp/crypto.py
+++ core/src/trezor/wire/thp/crypto.py
@@ -1,4 +1,7 @@
+from micropython import const  # pyright: ignore[reportMissingModuleSource]
+
 DUMMY_TAG = b"\xA0\xA1\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xB0\xB1\xB2\xB3\xB4\xB5"
+PUBKEY_LENGTH = const(32)
 
 
 # TODO implement
