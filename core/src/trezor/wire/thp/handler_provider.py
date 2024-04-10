29de7161542bbc91d7e3768251435ac1665318ac
M1nd3r <petrsedlacek.km@seznam.cz>
2024-04-10 12:00:33 +0200

    refactor(core): clean session creation

---
+++ core/src/trezor/wire/thp/handler_provider.py
@@ -0,0 +1,16 @@
+from typing import TYPE_CHECKING
+
+from trezor import protobuf
+
+from apps.thp import create_session
+
+if TYPE_CHECKING:
+    from typing import Any, Callable, Coroutine
+
+    pass
+
+
+def get_handler(
+    msg: protobuf.MessageType,
+) -> Callable[[Any, Any], Coroutine[Any, Any, protobuf.MessageType]]:
+    return create_session.create_new_session
