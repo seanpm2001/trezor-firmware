b3e29bfd14b32f11cb02ad3073f25fa255935410
M1nd3r <petrsedlacek.km@seznam.cz>
2024-05-21 11:33:12 +0200

    feat(core): add credential key derivation and invalidation [note: the new key derivation is not used in the current pairing flow, yet]

--- core/src/trezor/wire/thp/credential_manager.py
+++ core/src/trezor/wire/thp/credential_manager.py
@@ -13,6 +13,30 @@ if __debug__:
     from trezor import log
 
 
+def derive_cred_auth_key() -> bytes:
+    from storage.device import get_cred_auth_key_counter, get_thp_secret
+
+    # derive the key using SLIP-21 https://github.com/satoshilabs/slips/blob/master/slip-0021.md
+    # the derivation path is m/"Credential authentication key"/(counter 4-byte BE)
+
+    S = get_thp_secret()
+    m = hmac(hmac.SHA512, key=b"Symmetric key seed", message=S).digest()
+    label = b"Credential authentication key"
+    cred_auth_node = hmac(hmac.SHA512, key=m[0:32], message=b"\x00" + label).digest()
+    counter = get_cred_auth_key_counter()
+    cred_auth_key = hmac(
+        hmac.SHA512, key=cred_auth_node[0:32], message=b"\x00" + counter
+    ).digest()[32:64]
+
+    return cred_auth_key
+
+
+def invalidate_cred_auth_key() -> None:
+    from storage.device import increment_cred_auth_key_counter
+
+    increment_cred_auth_key_counter()
+
+
 def issue_credential(
     cred_auth_key: bytes,
     host_static_pubkey: bytes,
