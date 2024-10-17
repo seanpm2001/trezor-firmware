d0bdcc810b8095d3349eb51bb5ba9a5f6b6f78d4
M1nd3r <petrsedlacek.km@seznam.cz>
2024-10-17 13:51:08 +0200

    feat(core): enable benchmark on bitcoin_only builds [no changelog]

--- core/src/apps/workflow_handlers.py
+++ core/src/apps/workflow_handlers.py
@@ -29,6 +29,12 @@ def _find_message_handler_module(msg_type: int) -> str:
     if __debug__ and msg_type == MessageType.LoadDevice:
         return "apps.debug.load_device"
 
+    # benchmark
+    if __debug__ and msg_type == MessageType.BenchmarkListNames:
+        return "apps.benchmark.list_names"
+    if __debug__ and msg_type == MessageType.BenchmarkRun:
+        return "apps.benchmark.run"
+
     if utils.USE_THP:
         from trezor.enums import ThpMessageType
 
@@ -210,12 +216,6 @@ def _find_message_handler_module(msg_type: int) -> str:
             return "apps.solana.get_address"
         if msg_type == MessageType.SolanaSignTx:
             return "apps.solana.sign_tx"
-
-        # benchmark
-        if __debug__ and msg_type == MessageType.BenchmarkListNames:
-            return "apps.benchmark.list_names"
-        if __debug__ and msg_type == MessageType.BenchmarkRun:
-            return "apps.benchmark.run"
 
     raise ValueError
 
