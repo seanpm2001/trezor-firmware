5a01ffa44e325475313622b64430dbc88cee3d7a
M1nd3r <petrsedlacek.km@seznam.cz>
2024-11-21 10:31:43 +0100

    fix: try fix fido2-test [no changelog]

--- python/src/trezorlib/_internal/emulator.py
+++ python/src/trezorlib/_internal/emulator.py
@@ -95,6 +95,15 @@ class Emulator:
             raise RuntimeError
         return self._client
 
+    @client.setter
+    def client(self, new_client: TrezorClientDebugLink) -> None:
+        """Setter for the client property to update _client."""
+        if not isinstance(new_client, TrezorClientDebugLink):
+            raise TypeError(
+                f"Expected a TrezorClientDebugLink, got {type(new_client).__name__}."
+            )
+        self._client = new_client
+
     def make_args(self) -> List[str]:
         return []
 
